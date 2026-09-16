ff29179e1aa29d4dcb679acf054f7a508952a90a
dlwoduq20-lgtm <dlwoduq20@gmail.com>
2026-09-16 16:42:21 +0900

    fix(ux): remove loading.tsx to eliminate page transition splash, bump sw to v16

--- src/app/(main)/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function MainLoading() {
-  return (
-    <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-coral">
-      {/* Ensures zero white flash when navigating to /home */}
-    </div>
-  );
-}
